82c2c2a9eed26730b13cce7948a5ad076801ea93
zinchuk0480 <zinchuk0480@gmail.com>
2022-05-04 20:56:29 +0300

    new style rules

--- static/rules.js
+++ static/rules.js
@@ -3,6 +3,7 @@ lessonLink = document.querySelectorAll('.lesson_link')
 menuList = document.querySelector('.menu_list')
 
 function addMenu(){
+	console.log('sldfkj')
     for (let i = 0; i < articleTitle.length; i++){
         menuList.innerHTML += `<li class="menu_element"> ${lessonLink[i].innerText} ${articleTitle[i].innerText} </li>`
     }
@@ -10,9 +11,12 @@ function addMenu(){
 addMenu()
 
 menuElement = document.querySelectorAll('.menu_element')
-console.log('menuElement', menuElement)
 for (let i = 0; i < menuElement.length; i++){
-    menuElement[i].onclick = function(){
-        window.scrollTo(articleTitle[i].getBoundingClientRect().y + window.pageYOffset - 100)
+  menuElement[i].onclick = function(){
+		for (let j = 0; j < menuElement.length; j++){
+			menuElement[j].classList.remove('active_menu_element')
+		}
+		menuElement[i].classList.add('active_menu_element')
+    window.scrollTo(0, articleTitle[i].getBoundingClientRect().y + window.pageYOffset - 100)
     }
 }
