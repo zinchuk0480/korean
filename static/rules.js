151640dda1c0b237d2f24aca874aefe71760516d
zinchuk0480 <zinchuk0480@gmail.com>
2022-05-05 01:19:08 +0300

    new style rules

--- static/rules.js
+++ static/rules.js
@@ -1,5 +1,6 @@
 articleTitle = document.querySelectorAll('.article_title')
 lessonLink = document.querySelectorAll('.lesson_link')
+article = document.querySelectorAll('article')
 menuList = document.querySelector('.menu_list')
 
 function addMenu(){
@@ -15,8 +16,10 @@ for (let i = 0; i < menuElement.length; i++){
   menuElement[i].onclick = function(){
 		for (let j = 0; j < menuElement.length; j++){
 			menuElement[j].classList.remove('active_menu_element')
+			article[j].classList.remove('active_article')
 		}
 		menuElement[i].classList.add('active_menu_element')
+		article[i].classList.add('active_article')
     window.scrollTo(0, articleTitle[i].getBoundingClientRect().y + window.pageYOffset - 100)
     }
 }
