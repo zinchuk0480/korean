0239f58a40e93b77d3bfa32f3734efe390c01ba3
zinchuk0480 <zinchuk0480@gmail.com>
2022-05-05 12:40:13 +0300

    new rules style and script

--- static/rules.js
+++ static/rules.js
@@ -2,6 +2,9 @@ articleTitle = document.querySelectorAll('.article_title')
 lessonLink = document.querySelectorAll('.lesson_link')
 article = document.querySelectorAll('article')
 menuList = document.querySelector('.menu_list')
+menuCaller = document.querySelector('.menu_caller')
+containerNav = document.querySelector('.container_nav')
+
 
 function addMenu(){
 	console.log('sldfkj')
@@ -22,4 +25,10 @@ for (let i = 0; i < menuElement.length; i++){
 		article[i].classList.add('active_article')
     window.scrollTo(0, articleTitle[i].getBoundingClientRect().y + window.pageYOffset - 100)
     }
+}
+
+
+menuCaller.onclick = function(){
+	containerNav.classList.toggle('container_nav_active')
+	menuCaller.classList.toggle('menu_caller_active')
 }
